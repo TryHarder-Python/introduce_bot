3da085bf01db229d9d1b6b925c6e04caeb1236a2
Gleb <g.maximenko@disoft.dev>
2023-06-07 19:00:58 +0300

    import fixes

--- bot/settings/__init__.py
+++ bot/settings/__init__.py
@@ -1,5 +1,5 @@
-from bot.settings.enviroments import *  # noqa: F403 F401
-from bot.settings.bot import *  # noqa: F403 F401
+from settings.enviroments import *  # noqa: F403 F401
+from settings.bot import *  # noqa: F403 F401
 
 
 
